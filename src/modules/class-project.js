cf19995ceae3751b8080b39a7994b5d86965d5e1
catalinbadila <112496548+cbd23@users.noreply.github.com>
2023-11-02 11:06:45 +0200

    Add a module that creates project objects.

---
+++ src/modules/class-project.js
@@ -0,0 +1,9 @@
+// class Project creates project objects
+
+export class Project {
+    constructor(name, description, todoList) {
+        this.name = name;
+        this.description = description;
+        this.todoList = todoList;
+    };
+};
